930544b7d0823944d0fa4196e359e67f7604493b
Nagulan24 <nagulan1107@gmail.com>
2026-05-29 15:26:50 +0530

    reverse pattern using string building

---
+++ reverse.py
@@ -0,0 +1,19 @@
+value="nagulan"
+reverse_value=""
+i=len(value)-1
+while i >= 0:
+    reverse_value +=value[i]
+    i-=1
+print(value)
+print(reverse_value)
+
+# string building is adding small piece by piece
+#eg :
+# n stores first in reverse value
+# a stores second in reverse value
+# l stores first in reverse value
+# u stores first in reverse value
+# g stores first in reverse value
+# a stores first in reverse value
+# n stores first in reverse value
+#nalugan step by step adding each letter
